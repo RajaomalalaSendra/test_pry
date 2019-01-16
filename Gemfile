fc4a1a26446442eb1e18367467794c4c331ef11d
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-16 10:47:16 +0300

    the adding other like all the stuff

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ ruby "2.5.3"
 gem "pry"
 gem 'dotenv'
 gem 'twitter'
+gem 'rubocop', '~> 0.57.2'
--- lib/our_tweets.rb
+++ lib/our_tweets.rb
@@ -8,11 +8,11 @@ Dotenv.load
 
 # quelques lignes qui appellent les clés d'API de ton fichier .env
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+  config.consumer_key        = ENV["TWITTER_API_KEY"]
+  config.consumer_secret     = ENV["TWITTER_API_SECRET"]
   config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
   config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
 end
 
 # ligne qui permet de tweeter sur ton compte
-client.update('Mon premier tweet en Ruby !!!!')
+client.update('Mon deuxieme tweet en Ruby !!!!:smiley:')
